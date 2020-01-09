c81f73e2c2557d717dfcbe093ccedc6c7e89a2af
Marcos Lira <liram1995@gmail.com>
2020-01-09 14:54:01 -0600

    Added CRUD Operations to staff member table

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,6 +3,76 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Users = require("./auth-model.js");
 
+router.get('/', (req,res) => {
+    Users.findAllStaff()
+    .then(staff => {
+        res.status(200).json(staff);
+    })
+    .catch(err => {
+        res.status(500).json({ message: "There was an error retrieving Staff list..."});
+    });
+});
+
+router.get('/:id', (req,res) => {
+    const { id } = req.params;
+
+    Users.findByStaffId(id)
+    .then(staff => {
+        if(staff)
+        {
+            res.status(200).json(staff);
+        }
+        else
+        {
+            res.status(404).json({ message: "Unable to find staff member by this ID..."});
+        }
+    })
+    .catch(err => {
+        res.status.json({ message: "There was an error retrieving this data..."});
+    });
+});
+
+router.delete('/:id', (req,res) => {
+    const { id } = req.params;
+
+    Users.removeStaff(id)
+    .then(deleted => {
+        if(deleted)
+        {
+            res.status(200).json({ removed: deleted});
+        }
+        else
+        {
+            res.status(404).json({ message: "could not fin staff member with this ID..."});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Error processing deletion of staff member..."});
+    });
+});
+
+router.put('/:id', (req,res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    Users.findByStaffId(id)
+    .then(staff => {
+        if(staff)
+        {
+            Users.updateStaff(changes, id)
+            .then(updatedStaff => {
+                res.status(200).json(updatedStaff);
+            })
+        }
+        else
+        {
+            res.status(404).json({ message: "Could not find staff member with given ID..."});
+        }
+    });
+});
+
+
+
 router.post("/register", (req, res) => {
     // implement registration
     let user = req.body;
